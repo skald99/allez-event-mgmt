d9bc15e23adad6bedd0c8dcc0125126607b26839
Santosh <saisantosh.408@gmail.com>
2022-04-01 20:33:45 -0400

    User and Event Models

--- server/config/mongoConnection.ts
+++ server/config/mongoConnection.ts
@@ -1,1 +1,12 @@
-console.log("Test22");
+import mongoDB from "mongodb";
+import 'dotenv/config';
+
+const {MONGO_URL} = process.env;
+
+export async function connectDB() {
+    const client: mongoDB.MongoClient = new mongoDB.MongoClient(MONGO_URL!);
+    await client.connect();
+
+    const db: mongoDB.Db = client.db("Allez_Event_DB");
+    return db;
+}
