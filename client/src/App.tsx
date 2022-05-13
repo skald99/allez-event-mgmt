9c330aeab20842a73f964f7933ee07d7cc338558
PrasanthTata <73434266+prasanth1107@users.noreply.github.com>
2022-05-13 12:27:51 -0400

    Navigation

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,18 +2,31 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import EventDashboard from './components/EventDashboard'
-import { BrowserRouter as Router, Route, Link,Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
 import NewEvent, { EventType } from './components/NewEvent';
 import Map from './components/Map';
+import Navigation from './components/Navigatoion';
+import Home from "./components/Home"
+import EventDetails from './components/EventDetails';
 
 function App() {
   return (
 
     <div className="App">
       <Router>
-      <div className='App-body'>
-        <NewEvent type={EventType.NEW}/>
-      </div>
+        <header >
+          <Navigation />
+        </header>
+        <div className='App-body'>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/createEvent' element={<NewEvent type={EventType.NEW}/>} />
+            <Route path='/eventDashBoard' element={<EventDashboard />} />
+            <Route path='/event/:eventId' element={<EventDetails />} />
+          </Routes>
+
+          {/* <NewEvent type={EventType.NEW}/> */}
+        </div>
       </Router>
     </div>
   );
