8986c93e364ec126b50d679db9f9eaed1c1750f6
Santosh <saisantosh.408@gmail.com>
2022-05-15 10:42:24 -0400

    Added session management for all routes

--- client/src/components/Auth.tsx
+++ client/src/components/Auth.tsx
@@ -1,12 +1,22 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import EventList from "./EventDashboard";
 import LoginForm from "./LoginForm";
 import SignupForm from "./SignupForm";
 
 const Auth = () => {
+    const navigation = useNavigate();
+    
+    const userStat = (status: number) => {
+        
+        if(status === 200) {
+            navigation("/eventDashBoard");
+        }
+    }
     return (
         <div className="grid grid-cols-2">
             <div>
-                <LoginForm className="overflow-y-auto transition-all ease-in blur-sm hover:filter-none place-content-center focus-within:filter-none"/>
+                <LoginForm className="overflow-y-auto transition-all ease-in blur-sm hover:filter-none place-content-center focus-within:filter-none" userStatus = {userStat}/>
             </div>
             <div>
                 <SignupForm className="overflow-y-auto transition-all ease-in blur-sm hover:filter-none place-content-center focus-within:filter-none"/>
